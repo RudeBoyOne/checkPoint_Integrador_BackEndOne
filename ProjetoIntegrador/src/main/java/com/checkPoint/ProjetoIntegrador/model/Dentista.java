c1f378755dc633f1eafab5237b8b4953771a6a32
EverSilverio <ever.silverio@gmail.com>
2022-09-01 22:02:10 -0300

    Criado Model Dentista - issue#5

--- ProjetoIntegrador/src/main/java/com/checkPoint/ProjetoIntegrador/model/Dentista.java
+++ ProjetoIntegrador/src/main/java/com/checkPoint/ProjetoIntegrador/model/Dentista.java
@@ -1,5 +1,16 @@
 package com.checkPoint.ProjetoIntegrador.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class Dentista {
-    //TODO
+    private Integer idDentista;
+    private String nome;
+    private String sobrenome;
+    private String matriculaCadastro;
+
 }
