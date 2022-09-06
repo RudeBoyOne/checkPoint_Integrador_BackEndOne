beef26296144b4237a8bc1f60537d0b23f36880c
Danmartins09 <danielmartins9@gmail.com>
2022-09-05 23:08:38 -0300

    create instancia e teste de consulta

--- ProjetoIntegrador/src/main/java/com/checkPoint/ProjetoIntegrador/model/Consulta.java
+++ ProjetoIntegrador/src/main/java/com/checkPoint/ProjetoIntegrador/model/Consulta.java
@@ -1,5 +1,19 @@
 package com.checkPoint.ProjetoIntegrador.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Consulta {
-    //TODO
+    private Integer idConsulta;
+    private Paciente paciente;
+    private Dentista dentista;
+    private LocalDateTime dataHoraConsulta;
 }
