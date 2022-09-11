2e7fc236f4f8f1f5de35671e43a86b04e83f18fb
Ewerton Lopes Pereira <ewertonlopespereira@gmail.com>
2022-09-11 11:43:59 -0300

    Implementação do repository, desenvolvimento e realização do teste CRUD para entidade IDentistaRepository - Issue#18.

---
+++ ProjetoIntegrador/src/test/java/com/checkPoint/ProjetoIntegrador/repository/DentistaTest.java
@@ -0,0 +1,58 @@
+package com.checkPoint.ProjetoIntegrador.repository;
+
+
+import com.checkPoint.ProjetoIntegrador.model.Dentista;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class DentistaTest {
+
+    @Autowired
+    private IDentistaRepository dentistaRepository;
+
+    private Dentista dentista;
+
+    private Dentista dentistaSalvo;
+
+    @Test
+    public void criarDentista() {
+        this.dentista = new Dentista("Ewerton", "Lopes", "CRO-125987");
+        dentistaSalvo = dentistaRepository.save(dentista);
+        Assertions.assertNotNull(this.dentistaSalvo.getIdDentista());
+        Assertions.assertEquals(this.dentista.getNome(), this.dentistaSalvo.getNome());
+        Assertions.assertEquals(this.dentista.getSobrenome(), this.dentistaSalvo.getSobrenome());
+        Assertions.assertEquals(this.dentista.getMatriculaCadastro(), this.dentistaSalvo.getMatriculaCadastro());
+    }
+
+    @Test
+    public void buscarDentista() {
+        this.dentista = new Dentista("Ewerton", "Lopes", "CRO-125987");
+        dentistaSalvo = dentistaRepository.save(dentista);
+        Assertions.assertTrue(this.dentistaRepository.findById(this.dentistaSalvo.getIdDentista()).isPresent());
+    }
+
+    @Test
+    public void atualizarDentista() {
+        this.dentista = new Dentista("Ewerton", "Lopes", "CRO-125987");
+        dentistaSalvo = dentistaRepository.save(dentista);
+        this.dentistaSalvo.setNome("David");
+        this.dentistaSalvo.setSobrenome("Coverdale");
+        this.dentistaSalvo.setMatriculaCadastro("CRO-123789");
+        this.dentistaRepository.save(this.dentistaSalvo);
+        this.dentistaSalvo = this.dentistaRepository.findById(this.dentistaSalvo.getIdDentista()).get();
+        Assertions.assertEquals("David", this.dentistaSalvo.getNome());
+        Assertions.assertEquals("Coverdale", this.dentistaSalvo.getSobrenome());
+        Assertions.assertEquals("CRO-123789", this.dentistaSalvo.getMatriculaCadastro());
+    }
+
+    @Test
+    public void deletarDentista() {
+        this.dentista = new Dentista("Ewerton", "Lopes", "CRO-125987");
+        dentistaSalvo = dentistaRepository.save(dentista);
+        this.dentistaRepository.deleteById(this.dentistaSalvo.getIdDentista());
+        Assertions.assertFalse(this.dentistaRepository.findById(this.dentistaSalvo.getIdDentista()).isPresent());
+    }
+}
